958a8b637e03d2505f5b9dab0269d721c037090c
LeoGuimas <gleo.dev0@gmail.com>
2024-12-16 11:20:38 -0300

    Add Home component, Layout structure, and configure routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import {Layout} from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 const [movies, setMovies] = useState();
@@ -28,6 +30,11 @@ useEffect(() => {
   return (
     <div className="App">
 
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Home/>}></Route>
+      </Route>
+    </Routes>
     </div>
   );
 }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://6397-2804-a80-1041-e75c-d502-b5b3-2758-532b.ngrok-free.app',
+    baseURL: 'https://4af3-206-42-59-176.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
   });
   
